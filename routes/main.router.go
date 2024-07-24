362b29e0b91188775d5e2613725bef249bb2696c
wahyukrisnaaji32 <wahyukrisnaaji32@gmail.com>
2024-07-24 14:42:40 +0700

    perf: add cors

--- routes/main.router.go
+++ routes/main.router.go
@@ -3,11 +3,21 @@ package routes
 import (
 	"github.com/aliftech/todo-api/controllers"
 	"github.com/aliftech/todo-api/utils"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func MainRouter() {
 	r := gin.Default()
+
+	// Configure CORS options
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:8000"}, // Replace with your frontend URL
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
 	// Auth Router
 	r.POST("/signup", controllers.Signup)
